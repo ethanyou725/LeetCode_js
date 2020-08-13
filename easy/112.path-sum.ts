eec61c84496d5436ba3f8ed8d2cefdeb5a6acbd9
youzhigang <youzhigang@suiyi.com.cn>
2020-08-13 20:29:11 +0800

    temp

---
+++ easy/112.path-sum.ts
@@ -0,0 +1,28 @@
+// https://leetcode-cn.com/problems/path-sum/
+/**
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ */
+
+import { TreeNode } from "../typed";
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, sum: number): boolean {
+
+  function preOrder (root: TreeNode | null) {
+    console.log()
+  }
+  return false
+}
